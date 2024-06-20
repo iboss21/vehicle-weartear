f991c7b7d32fcc07da7fd022719f98e1a7858664
Master Mind <48193931+iboss21@users.noreply.github.com>
2024-06-19 22:18:07 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'iBoss21 | https://discord.gg/theluxempire'
+description 'Dynamic Vehicle Wear and Tear System for Realistic Roleplay'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+    'client/client.lua',
+}
+
+server_scripts {
+    'server/server.lua'
+}
