8abbbaf50b1668d3712e5f6242b9a140dc052418
Master Mind <48193931+iboss21@users.noreply.github.com>
2024-06-22 10:29:40 -0400

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,22 +1,44 @@
-local Framework = exports[Config.Framework == 'QBCore' and 'qb-core' or 'es_extended']:getSharedObject()
+local Framework, QBCore, ESX = nil, nil, nil
+
+-- Framework Detection (on resource start)
+Citizen.CreateThread(function()
+    while true do
+        if GetResourceState('qb-core') == 'started' then
+            QBCore = exports['qb-core']:GetCoreObject()
+            Framework = QBCore 
+            print("^2Vehicle Wear script is using QBCore^0")
+            break 
+        elseif GetResourceState('es_extended') == 'started' then
+            ESX = exports["es_extended"]:getSharedObject()
+            Framework = ESX
+            print("^2Vehicle Wear script is using ESX^0")
+            break 
+        end
+        Wait(100)
+    end
+end)
 
 RegisterServerEvent('theluxempire-vehiclewear:server:RepairVehicle')
 AddEventHandler('theluxempire-vehiclewear:server:RepairVehicle', function(vehicleNetId, repairCost)
     local src = source
-    local Player = Framework.Functions.GetPlayer(src)
-
-    -- Check money (adjust for ESX if needed)
-    if (Config.Framework == 'QBCore' and Player.Functions.RemoveMoney('cash', repairCost, "vehicle-repair")) or (Config.Framework == 'ESX' and Player.removeMoney(repairCost)) then
-        TriggerClientEvent('theluxempire-vehiclewear:client:VehicleRepaired', src, vehicleNetId)
-        TriggerEvent('QBCore:Notify', "Vehicle repaired!", 'success') 
-    else
-        TriggerEvent('QBCore:Notify', "Not enough money to repair!", 'error')
+    
+    if Framework ~= nil then
+        local Player = Framework.Functions.GetPlayer(src)
+    
+        if Framework.__framework == "es_extended" then
+            if Player.removeMoney(repairCost) then
+                TriggerClientEvent('theluxempire-vehiclewear:client:VehicleRepaired', src, vehicleNetId)
+                TriggerClientEvent('esx:showNotification', src, 'Vehicle repaired!')
+            else
+                TriggerClientEvent('esx:showNotification', src, 'Not enough money to repair!')
+            end
+        elseif Framework.__framework == "qb-core" then
+            if Player.Functions.RemoveMoney('cash', repairCost, "vehicle-repair") then
+                TriggerClientEvent('theluxempire-vehiclewear:client:VehicleRepaired', src, vehicleNetId)
+                TriggerClientEvent('QBCore:Notify', src, "Vehicle repaired!", "success")
+            else
+                TriggerClientEvent('QBCore:Notify', src, "Not enough money to repair!", "error")
+            end
+        end
     end
-end)
-
-RegisterNetEvent('theluxempire-vehiclewear:client:VehicleRepaired')
-AddEventHandler('theluxempire-vehiclewear:client:VehicleRepaired', function(vehicleNetId)
-    local vehicle = NetToVeh(vehicleNetId)
-    vehicleWear[vehicleNetId] = 0.0
-    SetVehicleDirtLevel(vehicle, 0.0)
 end)
