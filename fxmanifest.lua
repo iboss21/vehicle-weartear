0de8e8303648d9def090de51f1df1bc5bba25b2e
Master Mind <48193931+iboss21@users.noreply.github.com>
2024-06-22 10:31:49 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,14 +6,15 @@ author 'iBoss21 | https://discord.gg/theluxempire'
 description 'Dynamic Vehicle Wear and Tear System for Realistic Roleplay (Compatible with QBCore and ESX)'
 version '1.2.0'
 
-shared_scripts {
-    'config.lua'  -- Configuration file
+shared_scripts { 
+    '@qb-core/shared/locale.lua', -- For qb-core language support
+    'config.lua'       -- Configuration file
 }
 
 client_scripts {
-    'client/main.lua' 
+    'client.lua'         -- Client-side script (main.lua is now renamed to client.lua)
 }
 
 server_scripts {
-    'server/main.lua'
+    'server.lua'        -- Server-side script (main.lua is now renamed to server.lua)
 }
